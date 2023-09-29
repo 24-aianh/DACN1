23fdea7a635a1b33628bba3f17d9185479ccbdc3
24-aianh <20211813@eaut.edu.vn>
2023-09-29 17:35:26 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set("views", "./src/views");
 app.use(express.static('public'));
-
-app.use('/test', require('./controller/customer/testhome/api'));
+const helpers = require('./until/helper');
+const sql = require("mssql/msnodesqlv8");
 
 app.get('/', async (req, res) => {
-    console.log("trang chủ");
-    res.send("trang chủ");
+    
+    const query = "SELECT * from Account";
+    var a = await helpers.query(query);
+    // console.log(a.recordset);
+    res.render("home", {result: a});
+    // 
+    //kimanh
+    
 });
-//thanh
 
 app.listen(3000, () => {
     console.log(`127.0.0.1:3000`);
